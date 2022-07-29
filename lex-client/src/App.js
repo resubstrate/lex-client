105e680a8b57281a7d63522e96ca33707256ef5f
Thomas Chen <thomas@MacBook-Pro.local>
2022-07-29 00:40:10 -0700

    wip - base register,login, auth flow views

---
+++ lex-client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Register from './components/Register'
+import Login from './components/Login'
+
+function App() {
+  return (
+    <main className="App">
+      <Login/>
+    </main>
+  );
+}
+
+export default App;
