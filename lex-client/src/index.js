d01fa5c88869bd46e918bcd7539a80eee292eb42
tolmekian1453 <morshu1337@gmail.com>
2022-08-05 17:49:11 -0700

    hash router

--- lex-client/src/index.js
+++ lex-client/src/index.js
@@ -4,18 +4,18 @@ import './index.css';
 import App from './App';
 import { AuthProvider } from './context/AuthProvider';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <Routes>
           <Route path="/*" element={<App/>}/>
         </Routes>
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
